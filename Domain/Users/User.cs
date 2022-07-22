1ccc21de58af4bbc0a8f97e2e461b182644ebbee
maertjonas <maertensjonas@outlook.com>
2022-07-22 15:13:01 +0200

    Create User Entity

---
+++ Domain/Users/User.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Domain.Users
+{
+    public class User
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = Guard.Against.NullOrWhiteSpace(value, nameof(firstName)); }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = Guard.Against.NullOrWhiteSpace(value, nameof(lastName)); }
+        }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = Guard.Against.NullOrWhiteSpace(value, nameof(email)); }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = Guard.Against.NullOrWhiteSpace(value, nameof(password)); }
+        }
+        private DateTime dateOfBirth;
+        public DateTime DateOfBirth { get; set; }
+
+
+
+    }
+}
