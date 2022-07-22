29181e6078aadf86b765a0c86b3e5c6ba8b4fa20
maertjonas <maertensjonas@outlook.com>
2022-07-22 15:47:14 +0200

    Create Post Entity

---
+++ Domain/Posts/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ardalis.GuardClauses;
+
+namespace Domain.Posts
+{
+    public class Post
+    {
+        private bool pinned;
+        public bool Pinned { get; set; }
+
+        private bool archive;
+        public bool Archive { get; set; }
+
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = Guard.Against.NullOrWhiteSpace(value, nameof(text)); }
+        }
+
+        private DateTime date;
+        public DateTime Date { get; set; }
+        
+        private string image;
+        public string Image { get; set; }
+
+    }
+}
