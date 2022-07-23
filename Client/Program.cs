aadb514379c8fe643444beb4753dd05e8c350d75
maertjonas <maertensjonas@outlook.com>
2022-07-23 20:59:25 +0200

    FakeAuthorization Infrastructure

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Faith.Client;
 using Faith.Shared.Posts;
+using Microsoft.AspNetCore.Components.Authorization;
+using Faith.Client.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, FakeAuthenticationProvider>();
 builder.Services.AddScoped<IPostService, FakePostService>();
 await builder.Build().RunAsync();
---
+++ Client/Shared/FakeAuthenticationProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Faith.Client.Shared
+{
+    public class FakeAuthenticationProvider : AuthenticationStateProvider
+    {
+        public static ClaimsPrincipal Anonymous => new(new ClaimsIdentity());
+        public static ClaimsPrincipal Administrator => new(new ClaimsIdentity(new[]
+        {
+        new Claim(ClaimTypes.Name, "Fake Administrator"),
+        new Claim(ClaimTypes.Email, "fake-administrator@gmail.com"),
+        new Claim(ClaimTypes.Role, "Administrator"),
+    }, "Fake Authentication"));
+
+        public static ClaimsPrincipal Customer => new(new ClaimsIdentity(new[]
+        {
+        new Claim(ClaimTypes.Name, "Fake Customer"),
+        new Claim(ClaimTypes.Email, "fake-customer@gmail.com"),
+        new Claim(ClaimTypes.Role, "Customer"),
+    }, "Fake Authentication"));
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(Customer));
+        }
+    }
+}
