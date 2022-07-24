91a54cc020ee19c45b68edb4c67cefe255552855
maertjonas <maertensjonas@outlook.com>
2022-07-24 21:13:29 +0200

    Create User dto

---
+++ Shared/Users/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faith.Shared.Users
+{
+    public static class UserDto
+    {
+        public class Index
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
