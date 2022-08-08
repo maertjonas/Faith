b402cc22e39bda16f7189286a5c88cc171d37c2d
Maert <maertensjonas@outlook.com>
2022-08-08 11:55:16 +0200

    Create PostContext

---
+++ Services/Data/PostContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Posts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Data
+{
+    public class PostContext : DbContext
+    {
+        public PostContext(DbContextOptions<PostContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Post> Posts => Set<Post>();
+    }
+}
