5b40a776b57aea927c36b3b45911641381dc186c
Maert <maertensjonas@outlook.com>
2022-08-09 17:13:17 +0200

    Refactor UserService

--- Services/Users/UserService.cs
+++ Services/Users/UserService.cs
@@ -14,38 +14,27 @@ namespace Services.Users
             _context = context;
         }
 
-        public async Task<IEnumerable<UserDto.Index>> GetIndexAsync()
-        {
-            await Task.Delay(100);
-            return _context.Users.AsNoTracking().Select(u => new UserDto.Index
-            {
-                Id = u.Id,
-                FirstName = u.FirstName,
-                LastName = u.LastName
-            });
-        }
-
-        public async Task<UserDto.Index> GetDetailAsync(int userId)
+        public Task<int> CreateAsync(UserDto.Create model)
         {
             throw new NotImplementedException();
         }
 
-        public async Task DeleteAsync(int userId)
+        public Task<List<UserDto.Index>> GetIndexAsync()
         {
             throw new NotImplementedException();
         }
 
-        public async Task<int> CreateAsync(UserDto.Create model)
+        public Task<UserDto.Index> GetIndexAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int userId)
         {
             throw new NotImplementedException();
         }
 
         public Task UpdateAsync(UserDto.Update model)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<UserDto.Detail> IUserService.GetDetailAsync(int userId)
         {
             throw new NotImplementedException();
         }
